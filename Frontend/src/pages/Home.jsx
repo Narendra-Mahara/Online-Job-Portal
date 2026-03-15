5637f0cb25f509ad15226fe2c768a5c94577c213
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-15 11:00:03 +0545

    FIx Home component layout and styling for improved user experience

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,35 +1,37 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 px-4">
-      
-      <div className="text-center max-w-2xl">
+    <>
+      <div className="flex items-center justify-center  py-36 bg-gray-100 px-4">
+        <div className="text-center max-w-2xl">
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900">
+            Find Your <span className="text-blue-600">Dream Job</span> Today
+          </h1>
 
-        <h1 className="text-4xl md:text-5xl font-bold text-gray-900">
-          Find Your <span className="text-indigo-600">Dream Job</span> Today
-        </h1>
+          <p className="mt-4 text-gray-500 text-lg">
+            Connect with top employers, build your resume, and track
+            applications — all in one place.
+          </p>
 
-        <p className="mt-4 text-gray-500 text-lg">
-          Connect with top employers, build your resume, and track applications
-          — all in one place.
-        </p>
+          <div className="mt-8 flex justify-center gap-4 flex-wrap">
+            <Link to="/jobs">
+              {" "}
+              <button className="bg-blue-600 text-white px-6 py-3 rounded-lg transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-lg cursor-pointer">
+                Browse Jobs
+              </button>
+            </Link>
 
-        <div className="mt-8 flex justify-center gap-4 flex-wrap">
-
-          <button className="bg-indigo-600 text-white px-6 py-3 rounded-lg hover:bg-indigo-700">
-            🔍 Browse Jobs
-          </button>
-
-          <button className="border border-gray-300 px-6 py-3 rounded-lg hover:bg-gray-100">
-            Get Started Free
-          </button>
-
+            <Link to="/register">
+              {" "}
+              <button className="border border-gray-300 px-6 py-3 rounded-lg  transition-all duration-300  hover:scale-105 hover:shadow-lg  cursor-pointer">
+                Get Started
+              </button>
+            </Link>
+          </div>
         </div>
-
       </div>
-
-    </div>
+    </>
   );
 };
 
