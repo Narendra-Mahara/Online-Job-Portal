93aca924141677e1dcd8b8f4725ba8ef48ad8232
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-20 11:26:36 +0545

    add logout functionality to user controller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -107,4 +107,27 @@ const loginUser = async (req, res) => {
     );
 };
 
-export { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    },
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+};
+
+export { registerUser, loginUser, logoutUser };
