0674cd1216a4a04466f168959bfaa2a54c482338
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-21 22:51:14 +0545

    add getAllJobs route

--- Backend/src/routes/job.routes.js
+++ Backend/src/routes/job.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createJob } from "../controllers/job.controller.js";
+import { createJob, getAllJobs } from "../controllers/job.controller.js";
 import { isEmployer, jwtVerify } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/create").post(jwtVerify, isEmployer, createJob);
+router.route("/").get(getAllJobs);
 
 export default router;
