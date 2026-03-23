3f44903180988e211ab3664fd75e6ea7b8287329
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-23 09:02:05 +0545

    add updateJobStatus route

--- Backend/src/routes/job.routes.js
+++ Backend/src/routes/job.routes.js
@@ -4,6 +4,7 @@ import {
   getAllJobs,
   getEmployerJob,
   getJobById,
+  updateJobStatus,
 } from "../controllers/job.controller.js";
 import { isEmployer, jwtVerify } from "../middleware/auth.middleware.js";
 
@@ -12,5 +13,6 @@ const router = Router();
 router.route("/create").post(jwtVerify, isEmployer, createJob);
 router.route("/").get(getAllJobs);
 router.route("/employer-job").get(jwtVerify, isEmployer, getEmployerJob);
+router.route("/update/:id").put(jwtVerify, isEmployer, updateJobStatus);
 router.route("/:id").get(getJobById);
 export default router;
