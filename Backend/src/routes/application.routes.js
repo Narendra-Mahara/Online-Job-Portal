be4a2181ade8f8562869fe42b20ff8ec484369e1
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-24 09:37:16 +0545

    add getMyApplications route

--- Backend/src/routes/application.routes.js
+++ Backend/src/routes/application.routes.js
@@ -1,8 +1,15 @@
 import Router from "express";
-import { applyForJob } from "../controllers/application.controller.js";
-import { isJobseeker, jwtVerify } from "../middleware/auth.middleware.js";
+import {
+  applyForJob,
+  getMyApplications,
+} from "../controllers/application.controller.js";
+import {
+  isJobseeker,
+  jwtVerify,
+} from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/apply/:id").post(jwtVerify, isJobseeker, applyForJob);
+router.route("/my-applications").get(jwtVerify, isJobseeker, getMyApplications);
 export default router;
