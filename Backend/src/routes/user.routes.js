c682e68af573e2f91ec81123d68c4e2ebf49398e
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-25 09:18:09 +0545

    add multer middleware

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -6,11 +6,14 @@ import {
   updateProfile,
 } from "../controllers/user.controller.js";
 import { jwtVerify } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(jwtVerify, logoutUser);
-router.route("/update-profile").post(jwtVerify, updateProfile);
+router
+  .route("/update-profile")
+  .post(jwtVerify, upload.single("profileImage"), updateProfile);
 export default router;
