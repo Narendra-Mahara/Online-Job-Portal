30f44e4db04f0d0f25a9d46c3983247e22f4ebd4
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-26 08:41:36 +0545

    add deleteResume route

--- Backend/src/routes/resume.routes.js
+++ Backend/src/routes/resume.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { isJobseeker, jwtVerify } from "../middleware/auth.middleware.js";
-import { createResume } from "../controllers/resume.controller.js";
+import {
+  createResume,
+  deleteResume,
+} from "../controllers/resume.controller.js";
+
 const router = Router();
 
 router.route("/create").post(jwtVerify, isJobseeker, createResume);
+router.route("/delete/:resumeId").delete(jwtVerify, isJobseeker, deleteResume);
 export default router;
