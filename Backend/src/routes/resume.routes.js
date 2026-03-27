3562c8e51860bc73c9cd8d5a063e575b14296102
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-03-27 19:39:42 +0545

    add getResume route

--- Backend/src/routes/resume.routes.js
+++ Backend/src/routes/resume.routes.js
@@ -3,10 +3,12 @@ import { isJobseeker, jwtVerify } from "../middleware/auth.middleware.js";
 import {
   createResume,
   deleteResume,
+  getResume,
 } from "../controllers/resume.controller.js";
 
 const router = Router();
 
 router.route("/create").post(jwtVerify, isJobseeker, createResume);
 router.route("/delete/:resumeId").delete(jwtVerify, isJobseeker, deleteResume);
+router.route("/get-resume").get(jwtVerify, isJobseeker, getResume);
 export default router;
