2bd0ae69201e55012654a1ec14ba60a6b5e1529e
Niru Bohara <nirub3890@gmail.com>
2026-05-05 20:10:54 +0545

    feat(jobs): update job listing page and card design

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import dns from "dns"; // Set custom DNS servers to avoid potential DNS resolution issues
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
