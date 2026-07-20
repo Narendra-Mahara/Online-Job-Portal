1f77d6d6d0a829388d7763b7fd67e79029c62a0d
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-07-20 19:36:52 +0545

    add uploadResume functionality

--- Backend/src/controllers/resume.controller.js
+++ Backend/src/controllers/resume.controller.js
@@ -2,6 +2,9 @@ import { Resume } from "../models/resume.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import validator from "validator";
+import { PDFParse } from "pdf-parse";
+import { GoogleGenAI } from "@google/genai";
+
 const createResume = async (req, res) => {
   try {
     const { personalInfo, summary, education, experience, skills, projects } =
@@ -121,4 +124,105 @@ const deleteResume = async (req, res) => {
       .json(new ApiResponse(500, null, "Error while deleting resume", error));
   }
 };
-export { createResume, deleteResume, getResume, getResumeById };
+
+const uploadResume = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "Please upload a PDF file.",
+      });
+    }
+
+    const parser = new PDFParse({
+      data: req.file.buffer,
+    });
+
+    const result = await parser.getText();
+    await parser.destroy();
+    const resumeText = result.text;
+
+    const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+    const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+    const prompt = `
+Extract the following resume into the EXACT JSON schema.
+
+Rules:
+- Return ONLY valid JSON.
+- Do NOT use markdown.
+- Every field must exist.
+- Missing values should be "".
+- Missing arrays should be [].
+
+Schema:
+
+{
+  "personalInfo": {
+    "fullName": "",
+    "email": "",
+    "phone": "",
+    "address": "",
+    "linkedIn": "",
+    "github": ""
+  },
+  "summary": "",
+  "education": [
+    {
+      "institution": "",
+      "degree": "",
+      "fieldOfStudy": "",
+      "grade": "",
+      "startYear": "",
+      "endYear": ""
+    }
+  ],
+  "experience": [
+    {
+      "company": "",
+      "role": "",
+      "startDate": "",
+      "endDate": "",
+      "description": ""
+    }
+  ],
+  "skills": [""],
+  "projects": [
+    {
+      "title": "",
+      "description": "",
+      "link": ""
+    }
+  ]
+}
+
+Resume:
+
+${resumeText}
+`;
+    const response = await ai.models.generateContent({
+      model: "gemini-flash-lite-latest",
+      contents: prompt,
+    });
+
+    const jsonText = response.text
+      .replace(/```json/g, "")
+      .replace(/```/g, "")
+      .trim();
+
+    const extractedResume = JSON.parse(jsonText);
+
+    return res.status(200).json({
+      success: true,
+      message: "Resume extracted successfully.",
+      data: extractedResume,
+    });
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export { createResume, deleteResume, getResume, getResumeById, uploadResume };
