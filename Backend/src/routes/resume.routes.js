57a222f59adacd6a7e04fcc1fe8d23140d2abfc3
Narendra Singh Mahara <narendramahara9868@gmail.com>
2026-07-20 19:34:55 +0545

    add resume pdf upload route

--- Backend/src/routes/resume.routes.js
+++ Backend/src/routes/resume.routes.js
@@ -5,12 +5,17 @@ import {
   deleteResume,
   getResume,
   getResumeById,
+  uploadResume,
 } from "../controllers/resume.controller.js";
+import { uploadPdf } from "../middleware/pdfUpload.middleware.js";
 
 const router = Router();
 
 router.route("/create").post(jwtVerify, isJobseeker, createResume);
 router.route("/delete/:resumeId").delete(jwtVerify, isJobseeker, deleteResume);
 router.route("/get-resume").get(jwtVerify, isJobseeker, getResume);
+router
+  .route("/upload-pdf")
+  .post(jwtVerify, isJobseeker, uploadPdf.single("resume"), uploadResume);
 router.route("/:id").get(jwtVerify, isJobseeker, getResumeById);
 export default router;
